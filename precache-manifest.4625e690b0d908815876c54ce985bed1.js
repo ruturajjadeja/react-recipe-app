2b5d82a781382f461bc67288b54217a913a0f406
Ruturaj <rjadeja@umces.edu>
2019-04-17 15:36:59 -0400

    Updates

---
+++ precache-manifest.4625e690b0d908815876c54ce985bed1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9ed7916298e18513132",
+    "url": "/react-recipe-app/static/js/runtime~main.f6d3a2cf.js"
+  },
+  {
+    "revision": "f3c55f7846a5669a3539",
+    "url": "/react-recipe-app/static/js/main.f25ccc55.chunk.js"
+  },
+  {
+    "revision": "df155a8e9073ae43fd1c",
+    "url": "/react-recipe-app/static/js/2.9e03ac05.chunk.js"
+  },
+  {
+    "revision": "f3c55f7846a5669a3539",
+    "url": "/react-recipe-app/static/css/main.7eaac9b0.chunk.css"
+  },
+  {
+    "revision": "fdca2974fa2bd3f14b72d9829463a8a4",
+    "url": "/react-recipe-app/index.html"
+  }
+];
